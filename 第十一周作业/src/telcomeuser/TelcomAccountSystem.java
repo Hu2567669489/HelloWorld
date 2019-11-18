e897a5af7d0160c55665c84d1072548233dda898
unknown <you@example.com>
2019-11-18 23:42:28 +0800

    homework  11

---
+++ 第十一周作业/src/telcomeuser/TelcomAccountSystem.java
@@ -0,0 +1,12 @@
+package telcomeuser;
+
+public class TelcomAccountSystem {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		TelcomUser telcomUser = new TelcomUser("13800138000");
+		telcomUser.generateCommunicateRecord();
+		telcomUser.printDetails();
+	}
+
+}
